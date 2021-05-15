05f43424cbd951f88ec08445de922aeb1605180a
Qiming Sun <osirpt.sun@gmail.com>
2021-05-14 20:18:34 -0700

    test lyp screen 2

--- pyscf/dft/numint.py
+++ pyscf/dft/numint.py
@@ -853,7 +853,7 @@ def nr_rks(ni, mol, grids, xc_code, dms, relativity=0, hermi=0,
             aow = numpy.ndarray(ao[0].shape, order='F', buffer=aow)
             for idm in range(nset):
                 rho = make_rho(idm, ao, mask, 'GGA')
-                idx = rho[1:].sum(axis=0) < 1e-14
+                idx = (rho[0] < 1e-9) || (rho[1:].sum(axis=0) < 1e-9)
                 rho[:,idx] = 0
                 #print(rho[:,(rho[0] > 1e-10) & (rho[1:].sum(axis=0) < 1e-20)])
                 exc, vxc = ni.eval_xc(xc_code, rho, spin=0,
